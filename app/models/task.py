4403b271709dffb76d0dc81b94b3ab30e1868724
Anaiah <amaddox.dev@gmail.com>
2025-10-30 19:24:33 -0700

    added Task class, ran migrations

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,10 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    completed_at: Mapped[datetime] = mapped_column(default=None, nullable=True)
+    is_complete: Mapped[bool] = mapped_column(default=False, nullable=True)
